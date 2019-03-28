b3004237e9e592cab36de79d9bcdabcca8a08a9b
manjumaikan <maikan007@gmail.com>
2019-03-28 15:43:31 +1100

    modifications to set up script

--- mtail_init_script.sh
+++ mtail_init_script.sh
@@ -2,4 +2,4 @@
 
 mtail_root_dir=~/monitoring/tools/mtail
 cd $mtail_root_dir
-nohup ./mtail_v3.0.0-rc13_linux_amd64 -address localhost -logs /opt/IBM/WebSphere/AppServer/profiles/AppSrv01/logs/server1/SystemOut.log -progs mfile1.mtail -metric_push_interval_seconds 2 -port 3903 2>&1 &
+nohup ./mtail*amd64 -address localhost -logs ~/monitoring/dir_logging/dir_log_events.log -progs file_usage_log.mtail -metric_push_interval_seconds 2 -port 3002 2>&1 &
--- oakIT_monitoring_setup.sh
+++ oakIT_monitoring_setup.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
+echo "set up monitoring user"
+source_file_dir=$PWD
+sudo useradd -m oakitmonituser
+sudo groupadd oakitmonitgroup
+sudo passwd -d oakitmonituser
+sudo usermod -a -G oakitmonitgroup oakitmonituser
+mtail_root_dir="~/monitoring/tools/mtail"
+prometheus_root_dir="~/monitoring/tools/prometheus"
+alertmgr_root_dir="~/monitoring/tools/alertmanager"
+cloudwatchexpr_root_dir="~/monitoring/tools/cloudwatchexpr"
+
+chmod -R 755 *
+
+sudo su - << EOSA
+echo "oakitmonituser       ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+EOSA
+
+sudo su oakitmonituser << EOSU
 echo "set up mtail"
-mtail_root_dir=~/monitoring/tools/mtail
 mkdir -p $mtail_root_dir
 cd $mtail_root_dir
 wget https://github.com/google/mtail/releases/download/v3.0.0-rc29/mtail_v3.0.0-rc29_linux_amd64
-cp ~/file_usage_log.mtail ~/mtail_init_script.sh $mtail_root_dir
+sudo cp $source_file_dir/{file_usage_log.mtail,mtail_init_script.sh} $mtail_root_dir
 
 echo "set up prometheus"
-prometheus_root_dir=~/monitoring/tools/prometheus
 mkdir -p $prometheus_root_dir
 cd $prometheus_root_dir
 wget https://github.com/prometheus/prometheus/releases/download/v2.8.0/prometheus-2.8.0.linux-amd64.tar.gz
 tar xvzf prometheus-2.8.0.linux-amd64.tar.gz
 rm prometheus-2.8.0.linux-amd64.tar.gz
-cp ~/prometheus.yml ~/prometheus_init_script.sh $prometheus_root_dir/prometheus-2.8.0.linux-amd64/
+sudo cp $source_file_dir/{prometheus.yml,prometheus_init_script.sh} $prometheus_root_dir/prometheus-2.8.0.linux-amd64/
 
 echo "set up alertmanager"
-alertmgr_root_dir=~/monitoring/tools/alertmanager
 mkdir -p $alertmgr_root_dir
 cd $alertmgr_root_dir
 wget https://github.com/prometheus/alertmanager/releases/download/v0.15.0-rc.1/alertmanager-0.15.0-rc.1.linux-amd64.tar.gz --no-check-certificate
@@ -31,7 +46,6 @@ sudo /sbin/chkconfig --add grafana-server
 sudo systemctl restart grafana-server.service
 
 echo "set up cloudwatch exporter"
-cloudwatchexpr_root_dir=~/monitoring/tools/cloudwatchexpr
 mkdir -p $cloudwatchexpr_root_dir
 cd $cloudwatchexpr_root_dir
 wget https://github.com/prometheus/cloudwatch_exporter/archive/master.zip
@@ -39,3 +53,7 @@ unzip master.zip
 rm master.zip
 cd cloudwatch_exporter-master
 mvn package
+
+echo "set permission for monitoring folder"
+sudo chmod -R 755 ~/monitoring
+EOSU
